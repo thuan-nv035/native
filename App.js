e8d1a61ecb5f74e1492ea8855dabcbcab0a36275
thuan <thuan.nv035@gmail.com>
2022-08-10 21:02:19 +0700

    update

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet, ImageBackground } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
 import 'react-native-gesture-handler'
-import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
-  // const [count, setCount] = useState(0);
-  // const onPress = () => setCount(prevCount => prevCount + 1);
+
   return (
-    // <TailwindProvider>
-    //   <View className="flex-1 items-center justify-center bg-white">
-    //     <Text className="text-red-500">
-    //       Open up App.js to start working on your app!
-    //     </Text>
-    //     <StatusBar style="auto" />
-    //     <View>
-    //       <Text>count: {count}</Text>
-    //     </View>
-    //     <TouchableOpacity onPress={onPress} >
-    //       <Text className="text-red-500 font-bold px-4 py-2 shadow-sm">Click here</Text>
-    //     </TouchableOpacity>
-    //   </View>
-    // </TailwindProvider>
-    <NavigationContainer>
-      <View></View>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ImageBackground source={require('./assets/bg.jpg')}></ImageBackground>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  }
+})
