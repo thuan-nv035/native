04e362459f91c5e3542499504d9fd8a3362c2362
thuan <thuan.nv035@gmail.com>
2022-08-11 10:26:19 +0700

    add

--- App.js
+++ App.js
@@ -1,19 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { Text, View, TouchableOpacity, StyleSheet, ImageBackground } from "react-native";
-import { TailwindProvider } from "tailwindcss-react-native";
-import 'react-native-gesture-handler'
-export default function App() {
+import React from "react";
+import 'react-native-gesture-handler';
+import { createStackNavigator } from "@react-navigation/stack";
+import {  DefaultTheme, NavigationContainer } from "@react-navigation/native"
+import 'react-native-gesture-handler';
+import {useFonts} from 'expo-font'
 
+import Home from './screens/Home'
+import Details from "./screens/Details";
+
+const Stack = createStackNavigator()
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "transparent",
+  }
+}
+export default function App() {
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  });
+
+  if(!loaded) return null
   return (
-    <View style={styles.container}>
-      <ImageBackground source={require('./assets/bg.jpg')}></ImageBackground>
-    </View>
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName = "Homne">
+        <Stack.Screen name = "Home" component={Home} />
+        <Stack.Screen name = "Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-  }
-})
